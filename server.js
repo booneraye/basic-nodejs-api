d20f55eb5a669ffd00e2ea015f66e72ecd00c070
booneraye <booneraye@gmail.com>
2019-06-25 16:11:15 +0800

    add user post

--- server.js
+++ server.js
@@ -9,6 +9,25 @@ app.get('/listUser', function (req, res) {
 	});
 })
 
+
+var user = {
+	"user4" : {
+		"name" : "boone",
+		"password" : "password3",
+		"profession" : "clerk",
+		"id" : 4
+	}
+}
+
+app.post('/addUser', function (req, res){
+	fs.readFile(__dirname + "/" + "user.json", 'utf8', function (err, data1){
+		data1 = JSON.parse(data1);
+		data1["user4"] = user["user4"];
+		console.log(data1);
+		res.end(JSON.stringify(data1));
+	});
+})
+
 var server = app.listen(8081, function (){
 	var host = server.address().address
 	var port = server.address().port
